a3b8c00e261a57086aaaea0173c62f4be6e4abb2
陈昱桥 <1741253832@qq.com>
2024-09-12 21:29:27 +0800

    chore 路径别名配置

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,16 +3,37 @@ import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  // ---------------- 主进程配置 ----------------
   main: {
-    plugins: [externalizeDepsPlugin()]
+    plugins: [externalizeDepsPlugin()], // 使用 externalizeDepsPlugin 插件，将依赖外部化
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'), // 定义路径别名
+        '@config': resolve('src/config'),
+        '@main': resolve('src/main'),
+        '@components': resolve('src/renderer/src/components')
+      }
+    }
   },
+  // ---------------- 预加载脚本配置 ----------------
   preload: {
-    plugins: [externalizeDepsPlugin()]
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'), // 定义路径别名
+        '@config': resolve('src/config'),
+        '@main': resolve('src/main'),
+        '@components': resolve('src/renderer/src/components')
+      }
+    },
+    plugins: [externalizeDepsPlugin()] // 使用 externalizeDepsPlugin 插件，将依赖外部化
   },
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'), // 定义路径别名
+        '@config': resolve('src/config'),
+        '@main': resolve('src/main'),
+        '@components': resolve('src/renderer/src/components')
       }
     },
     plugins: [vue()]
