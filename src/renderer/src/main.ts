fbb24d73ed8f031010381d6673aa9f96b9c95999
陈昱桥 <1741253832@qq.com>
2024-09-30 16:25:02 +0800

    feat 渲染进程 自定义window.log

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,12 +1,11 @@
-import './assets/main.css'
+// 初始化日志
+import { initLog } from '@renderer/plugins/core/console'
+initLog()
 
+import './assets/main.css'
 import { createApp } from 'vue'
 import router from '@renderer/router'
 import pinia from '@renderer/store'
 import App from './App.vue'
-import { initLog } from '@renderer/plugins/core/console'
-
-// 初始化日志
-initLog()
 
 createApp(App).use(router).use(pinia).mount('#app')
